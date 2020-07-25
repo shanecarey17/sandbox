0ccf97b5c281def17ee6dd9fe63413aae09b797f
Shane Carey <shane.carey@Shanes-iMac.local>
2020-07-25 12:53:03 -0500

    adding main

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+const ethers = require("@nomiclabs/buidler").ethers;
+const legos = require('@studydefi/money-legos').legos;
+
+const tokens = require('./tokens.js');
+const model = require('./model.js');
+const kyber = require('./kyber.js');
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const run = async () => {
+    await tokens.TokenFactory.init();
+
+    let kyberSwap = await kyber.create(legos.kyber.network.address);
+
+    let mdl = new model.Model(kyberSwap);
+
+    kyberSwap.onSwap( function() { mdl.updateRate.apply(mdl, arguments); } );
+
+    let currentBlock = await ethers.provider.getBlockNumber();
+
+    ethers.provider.resetEventsBlock(currentBlock - 100);
+
+    while (true) {
+        mdl.findBestRate();
+
+        await sleep(1000);
+    }
+}
+
+function main() {
+    try {
+        run();
+    } catch (e) {
+        throw e;
+    }
+}
+
+main();
