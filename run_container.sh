5904b650ba37b13ee7789704bc6ee66c42f9c975
EC2 Default User <ec2-user@ip-172-31-36-148.ec2.internal>
2020-09-11 14:46:33 +0000

    add key to docker ignore

--- run_container.sh
+++ run_container.sh
@@ -1,1 +1,1 @@
-docker run -d --restart=always --env-file vars.env -e PRIVATE_KEY=$(cat .mainnet.key) liquidator
+docker build -t liquidator . && docker run -d --restart=always --env-file vars.env -e PRIVATE_KEY=$(cat .mainnet.key) liquidator
