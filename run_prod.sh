0e6d21002929e61e8d9c7df80035d3676bf77a5d
EC2 Default User <ec2-user@ip-172-31-36-148.ec2.internal>
2020-10-02 17:01:27 +0000

    better scripts

--- run_prod.sh
+++ run_prod.sh
@@ -1,8 +1,8 @@
-docker kill $(cat container.txt)
+docker stop $(cat container.txt)
+docker rm $(cat container.txt)
 docker build -t liquidator .
 docker run \
 	-d \
-        --rm \
 	--restart=always \
 	--env-file vars.env \
 	-e PRIVATE_KEY=$(cat .mainnet.key) \
