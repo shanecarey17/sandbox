085617a5ec82a741e2ee85b3ebfaeada219df519
shanec <shanecarey17@gmail.com>
2020-10-02 11:53:02 -0500

    rm docker container automatically

--- run_prod.sh
+++ run_prod.sh
@@ -2,6 +2,7 @@ docker kill $(cat container.txt)
 docker build -t liquidator .
 docker run \
 	-d \
+        --rm \
 	--restart=always \
 	--env-file vars.env \
 	-e PRIVATE_KEY=$(cat .mainnet.key) \
